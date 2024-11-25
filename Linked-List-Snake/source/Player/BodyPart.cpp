21be282c908c33d72361d4a9c38ce5d3b4f0d0db
prateektomar123 <prateektomar005@gmail.com>
2024-11-26 00:30:09 +0530

    Add Screen Wrapping

--- Linked-List-Snake/source/Player/BodyPart.cpp
+++ Linked-List-Snake/source/Player/BodyPart.cpp
@@ -91,22 +91,22 @@ namespace Player
 
 	sf::Vector2i BodyPart::getNextPositionDown()
 	{
-		return sf::Vector2i(grid_position.x, grid_position.y + 1);
+		return sf::Vector2i(grid_position.x, (grid_position.y + 1) % (LevelModel::number_of_rows));
 	}
 
 	sf::Vector2i BodyPart::getNextPositionUp()
 	{
-		return sf::Vector2i(grid_position.x, grid_position.y - 1);
+		return sf::Vector2i(grid_position.x, (grid_position.y - 1 + (LevelModel::number_of_rows)) % (LevelModel::number_of_rows));
 	}
 
 	sf::Vector2i BodyPart::getNextPositionRight()
 	{
-		return sf::Vector2i(grid_position.x + 1, grid_position.y);
+		return sf::Vector2i((grid_position.x + 1) % (LevelModel::number_of_columns), grid_position.y);
 	}
 
 	sf::Vector2i BodyPart::getNextPositionLeft()
 	{
-		return sf::Vector2i(grid_position.x - 1, grid_position.y);
+		return sf::Vector2i((grid_position.x - 1 + LevelModel::number_of_columns) % (LevelModel::number_of_columns), grid_position.y);
 	}
 	void BodyPart::updatePosition()
 	{
