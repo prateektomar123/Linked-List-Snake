e7b13a0d277c3e288dacfbb5fb07127f42c6d843
prateektomar123 <prateektomar005@gmail.com>
2024-12-04 23:49:27 +0530

    Insertion at the Middle - DLL

--- Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
+++ Linked-List-Snake/source/LinkedListLib/DoubleLinked/DoubleLinkedList.cpp
@@ -54,5 +54,48 @@ namespace LinkedListLib
             static_cast<DoubleNode*>(new_node)->previous = cur_node;
             initializeNode(new_node, cur_node, Operation::TAIL);
         }
+        void DoubleLinkedList::insertNodeAtMiddle()
+        {
+            if (head_node == nullptr) {
+                insertNodeAtHead();             // If the list is empty, insert at the head.
+                return;
+            }
+
+            int midIndex = findMiddleNode();    // Use the existing function to find the middle index
+            insertNodeAtIndex(midIndex);             // Use the existing function to insert the node at the found index             
+        }
+
+        void DoubleLinkedList::insertNodeAtIndex(int index)
+        {
+            if (index < 0 || index >= linked_list_size) return;
+
+            if (index == 0)
+            {
+                insertNodeAtHead();
+                return;
+            }
+
+            Node* new_node = createNode();
+            int current_index = 0;
+            Node* cur_node = head_node;
+            Node* prev_node = nullptr;
+
+            while (cur_node != nullptr && current_index < index)
+            {
+                prev_node = cur_node;
+                cur_node = cur_node->next;
+                current_index++;
+            }
+
+            prev_node->next = new_node;
+            static_cast<DoubleNode*>(new_node)->previous = prev_node;
+            new_node->next = cur_node;
+            static_cast<DoubleNode*>(cur_node)->previous = new_node;
+
+            initializeNode(new_node, head_node, Operation::TAIL);
+            linked_list_size++;
+
+            shiftNodesAfterInsertion(new_node, cur_node, prev_node);
+        }
     }
 }
